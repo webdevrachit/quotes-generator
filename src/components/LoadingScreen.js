9f5a8e24f391d6512de1abf59a0dab72a3a8d81b
Rachit Tandon <72686780+webdevrachit@users.noreply.github.com>
2021-02-02 01:38:56 +0530

    Update LoadingScreen.js

--- src/components/LoadingScreen.js
+++ src/components/LoadingScreen.js
@@ -4,8 +4,8 @@ const LoadingScreen = () => {
     return (
         <div className="loadingScreen__container">
             <div className="text__details">Made by </div>
-            <a href="https://www.arccoder.in" target="_blank" rel="noreferrer">
-                Arc Coder | Harsh Patel
+            <a href="github.com/webdevrachit" target="_blank" rel="noreferrer">
+                Rachit Tandon
             </a>
         </div>
     );
